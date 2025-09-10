97a7c0565d437f640da07a8dc7310037e6d60e3a
Gaute Hope <gauteh@met.no>
2025-09-10 11:00:10 +0200

    template from trajan

---
+++ docs/source/conf.py
@@ -0,0 +1,176 @@
+import os, sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Norkyst'
+copyright = '2025 Norwegian Meteorological Institute'
+author = 'Norwegian Meteorological Institute'
+
+release = ''
+version = ''
+
+# -- General configuration
+
+# autoapi_type = 'python'
+# autoapi_dirs = [ '../../trajan' ]
+# autoapi_keep_files = False  # set to True when debugging autoapi generated files
+# autoapi_python_class_content = 'both'
+# autodoc_typehints = 'none'
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning,
+                        message='Matplotlib is currently using agg, which is a'
+                                ' non-GUI backend, so cannot show the figure.')
+sphinx_gallery_conf = {
+     'examples_dirs': '../../examples/',   # path to your example scripts
+     'gallery_dirs': './gallery',  # path to where to save gallery generated output,
+     'filename_pattern': '/example_(?!long_)',
+     'ignore_pattern': 'create_test',
+     'backreferences_dir': None,
+     'capture_repr': ('_repr_html_', '__repr__'),
+     'abort_on_example_error': False,
+     'thumbnail_size': (300, 300),
+     'junit': '../../test-results/sphinx-gallery/junit.xml',
+}
+
+extensions = [
+    #"sphinxcontrib.mermaid",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.napoleon",
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    #'autoapi.extension',
+    'sphinx.ext.autosummary',
+    # 'sphinx_autosummary_accessors',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'sphinx_gallery.gen_gallery',
+    #'numpydoc',
+    'matplotlib.sphinxext.plot_directive',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy', None),
+    'cftime': ('https://unidata.github.io/cftime', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'pyproj': ('https://pyproj4.github.io/pyproj/stable/', None),
+    'xarray': ('https://xarray.pydata.org/en/stable/', None),
+    'pandas': ("https://pandas.pydata.org/pandas-docs/stable", None),
+}
+#intersphinx_disabled_domains = ['std']
+
+# templates_path = ["_templates", sphinx_autosummary_accessors.templates_path]
+
+html_context = {
+    # "github_url": "https://github.com", # or your GitHub Enterprise site
+    "github_user": "norkyst",
+    "github_repo": "norkyst.github.io",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+
+html_theme_options = {
+    "use_edit_page_button": True,
+}
+
+autosummary_generate = True
+autodoc_typehints = "none"
+autodoc_typehints_description_target = "documented"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "private-members": True,
+}
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_ivar = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+napoleon_type_aliases = {
+    # general terms
+    "sequence": ":term:`sequence`",
+    "iterable": ":term:`iterable`",
+    "callable": ":py:func:`callable`",
+    "dict-like": ":term:`dict-like <mapping>`",
+    "path-like": ":term:`path-like <path-like object>`",
+    "mapping": ":term:`mapping`",
+    "file-like": ":term:`file-like <file-like object>`",
+    # special terms
+    # "same type as caller": "*same type as caller*",  # does not work, yet
+    # "same type as values": "*same type as values*",  # does not work, yet
+    # stdlib type aliases
+    "MutableMapping": "~collections.abc.MutableMapping",
+    "sys.stdout": ":obj:`sys.stdout`",
+    "timedelta": "~datetime.timedelta",
+    "string": ":class:`string <str>`",
+    # numpy terms
+    "array_like": ":term:`array_like`",
+    "array-like": ":term:`array-like <array_like>`",
+    "scalar": ":term:`scalar`",
+    "array": ":term:`array`",
+    "hashable": ":term:`hashable <name>`",
+    # matplotlib terms
+    "color-like": ":py:func:`color-like <matplotlib.colors.is_color_like>`",
+    "matplotlib colormap name": ":doc:`matplotlib colormap name <matplotlib:gallery/color/colormap_reference>`",
+    "matplotlib axes object": ":py:class:`matplotlib axes object <matplotlib.axes.Axes>`",
+    "colormap": ":py:class:`colormap <matplotlib.colors.Colormap>`",
+    # xarray terms
+    "dim name": ":term:`dimension name <name>`",
+    "var name": ":term:`variable name <name>`",
+    # objects without namespace: xarray
+    "DataArray": "~xarray.DataArray",
+    "Dataset": "~xarray.Dataset",
+    "Variable": "~xarray.Variable",
+    "DataTree": "~xarray.DataTree",
+    "DatasetGroupBy": "~xarray.core.groupby.DatasetGroupBy",
+    "DataArrayGroupBy": "~xarray.core.groupby.DataArrayGroupBy",
+    "Grouper": "~xarray.groupers.Grouper",
+    "Resampler": "~xarray.groupers.Resampler",
+    # objects without namespace: numpy
+    "ndarray": "~numpy.ndarray",
+    "MaskedArray": "~numpy.ma.MaskedArray",
+    "dtype": "~numpy.dtype",
+    "ComplexWarning": "~numpy.ComplexWarning",
+    # objects without namespace: pandas
+    "Index": "~pandas.Index",
+    "MultiIndex": "~pandas.MultiIndex",
+    "CategoricalIndex": "~pandas.CategoricalIndex",
+    "TimedeltaIndex": "~pandas.TimedeltaIndex",
+    "DatetimeIndex": "~pandas.DatetimeIndex",
+    "IntervalIndex": "~pandas.IntervalIndex",
+    "Series": "~pandas.Series",
+    "DataFrame": "~pandas.DataFrame",
+    "Categorical": "~pandas.Categorical",
+    "Path": "~~pathlib.Path",
+    # objects with abbreviated namespace (from pandas)
+    "pd.Index": "~pandas.Index",
+    "pd.NaT": "~pandas.NaT",
+}
+
+# numpydoc_show_class_members = False
+# Report warnings for all validation checks except the ones listed after "all"
+numpydoc_validation_checks = {"all", "ES01", "EX01", "SA01", "SA04"}
+# don't report on objects that match any of these regex
+numpydoc_validation_exclude = {
+    r"\.__repr__$",
+    # "cf_xarray.accessor.",
+    # "cf_xarray.set_options.",
+}
+
+# -- Options for HTML output
+
+html_theme = 'pydata_sphinx_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
---
+++ docs/source/index.rst
@@ -0,0 +1,88 @@
+Norkyst
+===================================
+
+TrajAn is a Python package with functionality to handle trajectory datasets following the `CF-conventions on trajectories <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.10/cf-conventions.html#trajectory-data>`_.
+
+Trajectory datasets contain position time series from e.g. drifting buoys, or output from lagrangian models.
+
+The source code is available on https://github.com/OpenDrift/trajan
+
+Installation
+------------
+
+**Install from internet package sources (recommended for users)**
+
+.. code-block:: console
+
+   $ conda install -c conda-forge trajan
+
+
+or
+
+.. code-block:: console
+
+   $ pip install trajan
+
+**Install from source (recommended to develop for Trajan)**
+
+.. code-block:: console
+
+   $ cd trajan  # move to the location of the trajan root, containing the pyproject.toml file
+   $ pip install .
+
+Remember to re-install each time you have done an edit during the development process.
+
+Usage
+-----
+
+*TrajAn* is an `Xarry extension <https://docs.xarray.dev/en/stable/internals/extending-xarray.html>`_. On drifter (or trajectory) datasets you can use the `.traj` accessor on `xarray.Dataset <https://docs.xarray.dev/en/stable/generated/xarray.Dataset.html>`_ s. In order to register the accessor, _trajan_ needs to be imported:
+
+.. code-block:: python
+
+   import matplotlib.pyplot as plt
+   import xarray as xr
+   import trajan as _
+
+   ds = xr.open_dataset('drifter_dataset.nc')
+
+   ds.traj.plot()
+   plt.show()
+
+   speed = ds.traj.speed()
+   print(f'Max speed {speed.max().values} m/s')
+
+
+Trajectory datasets from different models and observations tend to have many small differences. TrajAn expects the dataset to be `CF-compliant <http://cfconventions.org/Data/cf-conventions/cf-conventions-1.10/cf-conventions.html#trajectory-data>`_. However, the standard does leave some room for interpretation.
+
+Generally, TrajAn supports two types of data layout:
+  1) trajectories sampled at different times (unstructured or irregular grid), almost every dataset from real observations.
+  2) trajectories sampled at uniform (or regular) grid, typical the output from a model.
+
+We refer to the first type as _2D_ since time is a function of trajectory and observation, while the second type is _1D_ and time is only a function of observation.
+__TrajAn__ will detect which type of dataset you have and you will have access to the appropriate methods for the type data layout.
+
+While the first type (_2D_) is more general it often limits analysis that require trajectories to be sampled at the same points, you can therefor convert a _2D_ dataset to _1D_ by using :meth:`trajan.traj2d.Traj2d.gridtime`.
+
+Methods applicable to both types of datasets can be found in: :mod:`trajan.traj`, methods for _1D_ datasets: :mod:`trajan.traj.traj1d`, and _2D_: :mod:`trajan.traj.traj2d`. All methods are forwarded to the accessor, so you call the methods on `Dataset.traj`:
+
+.. code-block:: python
+
+   ds = ds.traj.gridtime('1H')   # grid dataset to every hour (a 2D method)
+   ds.traj.plot()                # plot dataset
+
+
+Generic plotting is available in the standard `Xarray` way, and strives to stay as close to `matplotlib` as possible: (:meth:`trajan.trajectory_accessor.TrajAccessor.plot`) and animation (:meth:`trajan.trajectory_accessor.TrajAccessor.animate`).
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+
+   user_guide/index
+   gallery/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
