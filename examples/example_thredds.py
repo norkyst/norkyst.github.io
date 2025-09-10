1566c6942cec41c6167fc26cbc8bd2cff63ca0f0
Gaute Hope <gauteh@met.no>
2025-09-10 11:18:20 +0200

    add example

---
+++ examples/example_thredds.py
@@ -0,0 +1,29 @@
+"""
+Read variables from a model along the trajectories of drifters.
+=============================================================================
+"""
+
+import xarray as xr
+import cf_xarray as _
+import pyproj
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+#%%
+# Open the Norkyst800 model
+nk = xr.open_dataset(
+    'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be')
+print(nk)
+
+#%%
+# Use cf-xarray to get the CRS
+gm = nk.cf['grid_mapping']
+nk_crs = pyproj.CRS.from_cf(gm.attrs)
+print(nk_crs)
+nk_ccrs = ccrs.Projection(nk_crs.to_proj4())
+
+#%%
+# Plot northward surface current
+plt.axes(projection=ccrs.Mercator())
+nk.isel(time=-1, depth=0).v_northward.plot.imshow() # TODO: SUPPLY CORRECT TRANSFORM HERE!
+plt.show()
